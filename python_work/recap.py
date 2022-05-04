9c9a52b89f510da91fb033ce80f2a4a3f5838402
Deborah Sanyaolu <debbie-ps@hotmail.co.uk>
2022-05-04 16:57:01 +0100

    activities

---
+++ python_work/recap.py
@@ -0,0 +1,56 @@
+import random
+# print is how we display information in a terminal
+print("this is my current file")
+
+# strings are for representing character
+print ("Hello my name is Klong")
+print("1234")
+
+#This is an integer - a whole number
+print(1234)
+
+#this is a floating point - anything with a decimal
+print(1234.5)
+
+#booleans - True and False
+print(True)
+print(False)
+
+#none - a blank, or a null type
+print(None)
+
+#len - provides how many characters in string
+print(len("hello  "))
+
+#index position - starts its count at 0
+print("hello"[-1])
+
+# .notation is a way of accessing methods
+print("hello".upper())
+
+print(random.random())
+print (random.uniform(1,10))
+print(random.randint(1,10))
+
+imort random
+from random import random, randint, uniform
+
+print(uniform())
+
+my_name = "Klong"
+my_age = 30
+
+print(my_name)
+
+print ("Hello my name is",my_name)
+print("Hello my name is" + my_name)
+print(f"Helo my name is {my_name} and I am {my_age} years old")
+
+#Arithmetic operators
+print(1+2)
+print(2-1)
+print(2*3)
+print(3**3)
+print(15/3)
+print(15%3)
+
